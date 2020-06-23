6daa0423619861991b40316724d6e047ca458bd6
CoolYK <391595714@qq.com>
2020-06-23 18:02:54 +0800

    pojo

---
+++ coolmall/coolmall-dao/src/main/java/com/coolyk/coolmall/mapper/TbBrandMapper.java
@@ -0,0 +1,4 @@
+package com.coolyk.coolmall.mapper;
+
+public class TbBrandMapper {
+}
---
+++ coolmall/coolmall-pojo/src/main/java/com/coolyk/coolmall/pojo/TbBrand.java
@@ -0,0 +1,32 @@
+package com.coolyk.coolmall.pojo;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author yekui
+ * @since 2019-11-14
+ */
+
+
+public class TbBrand implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    /**
+     * 品牌名称
+     */
+    private String name;
+
+    /**
+     * 品牌首字母
+     */
+    private String firstChar;
+
+
+}
